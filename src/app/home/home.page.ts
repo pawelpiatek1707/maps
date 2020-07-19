a54c933f9f4df0ca27f4969044052a6f88805041
Pawel Piatek <pawelpiatek98@gmail.com>
2020-07-19 16:20:03 +0200

    added points

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,6 +8,11 @@ import * as olProj from 'ol/proj';
 import VectorImageLayer from 'ol/layer/VectorImage';
 import GeoJSON from 'ol/format/GeoJSON';
 import VectorSource from 'ol/source/Vector';
+import Feature from 'ol/Feature';
+import Point from 'ol/geom/Point';
+import Style from 'ol/style/Style';
+import Icon from 'ol/style/Icon';
+import VectorLayer from 'ol/layer/Vector';
 
 
 @Component({
@@ -20,52 +25,49 @@ export class HomePage implements OnInit{
   x = 2101520.252157975;
   y = 6495746.574340763;
 
+  point1;
+
   map;
 
-  osmHumanitarian = new TileLayer({
-    source: new OSM({
-      url: 'http://a.tile.openstreetmap.fr/hot/${z}/${x}/${y}.png'
-    }),
-    visable: false,
-    title: 'osmHumanitarian'
-  })
+  vectorSource;
+  vectorLayer;
 
-  osmStandard = new TileLayer({
-    source: new OSM(),
-    visable: false,
-    title: 'osmStandard'
-  });
-
-  // points = VectorImageLayer({
-  //   source: new VectorSource({
-  //     url: '../../assets/data/map.geojason',
-  //     format: new GeoJSON()
-  //   }),
-  //   visible: true,
-  //   title: 'points'
-     
-  // })
-
-  // stamenTerrain = new TileLayer({
-  //   source: new XYZ({
-  //     url: 'http://tile.stamen.com/terrain/{z}/{x}/{y}.jpg',
-  //     attributions: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
-  //   }),
-  //   visable: true,
-  //   title: 'stamenTerrain'
-  // });
-
-  // baseLayerGroup = new TileLayer.Group({
-  //   layers: [this.osmStandard, this.osmHumanitarian, this.stamenTerrain]
-  // })
+ 
 
  
 
   constructor() {}
 
   ngOnInit() {
+    this.point1 = new Feature({
+      geometry: new Point([2101042.6887812307, 6496193.376841301])
+    })
+
+    this.point1.setStyle(new Style({
+      image: new Icon(({
+        color: '#8959A8',
+        crossOrigin: 'anonymous',
+        src: 'assets/pin.svg',
+        imgSize: [20,20]
+      }))
+    }));
+
+    this.vectorSource = new VectorSource({
+      features: [this.point1]
+    });
+
+    this.vectorLayer = new VectorLayer({
+      source: this.vectorSource
+    })
+
     this.map = new Map({
       target: 'hotel_map',
+      layers: [
+        new TileLayer({
+          source: new OSM()
+        }), 
+        this.vectorLayer
+      ],
       view: new View({
         center: ([this.x, this.y]),
         zoom: 14,
@@ -74,10 +76,6 @@ export class HomePage implements OnInit{
       })
     });
 
-    // this.map.addLayer(this.stamenTerrain);
-    // this.map.addLayer(this.osmHumanitarian);
-    this.map.addLayer(this.osmStandard);
-    // this.map.addLayer(this.points);
 
   }
 
