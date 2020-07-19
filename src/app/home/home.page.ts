06065b9f3cac3e4d348b62f54906c1ca1be5c09b
Pawel Piatek <pawelpiatek98@gmail.com>
2020-07-19 15:59:48 +0200

    before adding branch

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -5,6 +5,10 @@ import TileLayer from 'ol/layer/Tile';
 import XYZ from 'ol/source/XYZ';
 import OSM from 'ol/source/OSM';
 import * as olProj from 'ol/proj';
+import VectorImageLayer from 'ol/layer/VectorImage';
+import GeoJSON from 'ol/format/GeoJSON';
+import VectorSource from 'ol/source/Vector';
+
 
 @Component({
   selector: 'app-home',
@@ -13,25 +17,67 @@ import * as olProj from 'ol/proj';
 })
 export class HomePage implements OnInit{
 
-  map;
   x = 2101520.252157975;
   y = 6495746.574340763;
+
+  map;
+
+  osmHumanitarian = new TileLayer({
+    source: new OSM({
+      url: 'http://a.tile.openstreetmap.fr/hot/${z}/${x}/${y}.png'
+    }),
+    visable: false,
+    title: 'osmHumanitarian'
+  })
+
+  osmStandard = new TileLayer({
+    source: new OSM(),
+    visable: false,
+    title: 'osmStandard'
+  });
+
+  // points = VectorImageLayer({
+  //   source: new VectorSource({
+  //     url: '../../assets/data/map.geojason',
+  //     format: new GeoJSON()
+  //   }),
+  //   visible: true,
+  //   title: 'points'
+     
+  // })
+
+  // stamenTerrain = new TileLayer({
+  //   source: new XYZ({
+  //     url: 'http://tile.stamen.com/terrain/{z}/{x}/{y}.jpg',
+  //     attributions: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
+  //   }),
+  //   visable: true,
+  //   title: 'stamenTerrain'
+  // });
+
+  // baseLayerGroup = new TileLayer.Group({
+  //   layers: [this.osmStandard, this.osmHumanitarian, this.stamenTerrain]
+  // })
+
+ 
 
   constructor() {}
 
   ngOnInit() {
     this.map = new Map({
       target: 'hotel_map',
-      layers: [
-        new TileLayer({
-          source: new OSM()
-        })
-      ],
       view: new View({
         center: ([this.x, this.y]),
-        zoom: 14
+        zoom: 14,
+        // maxZoom: 
+        // minZoom
       })
     });
+
+    // this.map.addLayer(this.stamenTerrain);
+    // this.map.addLayer(this.osmHumanitarian);
+    this.map.addLayer(this.osmStandard);
+    // this.map.addLayer(this.points);
 
   }
 
